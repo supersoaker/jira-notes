a647581e50b96ff423ce004cec723c9f933d323c
marlon.ruescher <m.ruescher@gds-gmbh.de>
2017-09-09 15:12:39 +0200

    Added datepicker

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ import './helpers/external_links.js';
 import {remote} from 'electron';
 import jetpack from 'fs-jetpack';
 // import { greet } from './hello_world/hello_world';
-import {getProjects} from './jira/api';
+import {callApi} from './jira/api';
 import env from './env';
-
+const flatpickr = require("flatpickr");
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
 
@@ -24,11 +24,17 @@ const osMap = {
     linux: 'Linux',
 };
 
+flatpickr(".calendar input", {
+    // inline: truedefaultDate: [new Date(), "2016-10-30"]
+    defaultDate: new Date()
+});
+
+
 let showPage = (pageName) => {
     let otherPages = document.querySelectorAll('.main>[class$="-page"]');
     let pageElem = document.querySelector(`.main > .${pageName}-page`);
     otherPages.forEach((elem) => {
-        if(elem !== pageElem) {
+        if (elem !== pageElem) {
             elem.classList.add('hide');
         }
     });
@@ -37,7 +43,7 @@ let showPage = (pageName) => {
     }
 };
 let showError = (msg) => {
-    if(!msg) {
+    if (!msg) {
         document.querySelector('.message-wrapper').classList.add('hide');
         document.querySelector('.message-wrapper').classList.remove('error');
     } else {
@@ -48,35 +54,46 @@ let showError = (msg) => {
 };
 if (localStorage.getItem('jira-host')) {
     showPage('overview');
+    // let xhr = callApi(
+    //     localStorage.getItem('jira-host'),
+    //     localStorage.getItem('jira-user'),
+    //     localStorage.getItem('jira-pass')
+    // );
+    // localStorage.setItem('jira-projects', xhr.responseText);
+    // let projects = JSON.parse(xhr.responseText);
+    // let html = '';
+    // projects.forEach((project) => {
+    //     html +=  `<li>${project.name} (${project.key})</li>`;
+    // });
+    // document.getElementById('project-list').innerHTML = html;
 }
+
+
 document.querySelector('#sign-in-form').addEventListener('submit', (e) => {
     let form = new FormData(e.target);
     try {
         /** @var XMLHTTPRequest xhr */
-        let xhr = getProjects(form.get('host'), form.get('user'), form.get('pass'));
-        if (xhr.status == 200) {
+        let xhr = callApi(form.get('host'), form.get('user'), form.get('pass'));
+        if (xhr.status === 200) {
             localStorage.setItem('jira-host', form.get('host'));
             localStorage.setItem('jira-user', form.get('user'));
             localStorage.setItem('jira-pass', form.get('pass'));
+            localStorage.setItem('jira-projects', xhr.responseText);
+            showPage('overview');
         }
-        if (xhr.status == 401 || xhr.status == 403) {
+        if (xhr.status === 401 || xhr.status === 403) {
             showError('Bad credentials');
         }
-        if(xhr.responseText == '[]') {
+        if (xhr.responseText === '[]') {
             showError('Wrong url or no projects available');
         }
-        if(xhr.status === 0) {
+        if (xhr.status === 0) {
             showError('Error when accessing url');
         }
-        // if (client.status == 200)
-        //     alert("The request succeeded!\n\nThe response representation was:\n\n" + client.responseText)
-        // else
-        //     alert("The request did not succeed!\n\nThe response status was: " + client.status + " " + client.statusText + ".");
-        console.log(xhr);
     } catch (e) {
         showError('Error when accessing url');
-        console.log(e);
-        console.error(e);
+        // console.log(e);
+        // console.error(e);
     }
     e.preventDefault();
     return false;
--- src/jira/api.js
+++ src/jira/api.js
@@ -1,4 +1,21 @@
-export const getProjects = (url, username, password) => {
+export const callApi = (url, username, password, query = 'project') => {
+    let JSONObject = {"username": ""+username+"", "password": ""+password+""};
+
+    let client = new XMLHttpRequest();
+
+    url = `${url}/rest/api/2/${query}`;
+    // rest/api/2/project
+
+    // No async !!1
+    client.open("GET", url, false);
+    client.setRequestHeader("Content-Type", "application/json");
+    client.setRequestHeader("Authorization", 'Basic ' + btoa(`${username}:${password}`));
+
+    client.send(JSONObject);
+
+    return client;
+};
+export const getIssuesByProject = (project, url, username, password) => {
     let JSONObject = {"username": ""+username+"", "password": ""+password+""};
 
     let client = new XMLHttpRequest();
